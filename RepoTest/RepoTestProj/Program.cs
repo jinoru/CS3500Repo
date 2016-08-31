9431e4aa778723c5405c46fc2c2c2c8486e52496
teeples <jeromy.teeples@utah.edu>
2016-08-31 15:22:24 -0600

    Added program description and command to print to console

---
+++ RepoTest/RepoTestProj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoTestProj
+{
+    /// <summary>
+    /// Program to change to fool around with git
+    /// </summary>
+    class Lab2RepoTestProgram
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TestRepo");
+        }
+    }
+}
